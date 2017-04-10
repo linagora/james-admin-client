8551095ae6971c055983da25ca79bafa70e66800
Sang NGO DOAN <sngodoan@linagora.com>
2017-04-10 11:47:01 +0700

    gulp: add new task to release new version

--- README.md
+++ README.md
@@ -8,6 +8,20 @@
 
 TODO
 
+## Release
+
+To release new patch version, run this command on `master` branch:
+
+`gulp release`
+
+New minor version:
+
+`gulp release --minor`
+
+New major version:
+
+`gulp release --major`
+
 ## Licence
 
 [Affero GPL v3](http://www.gnu.org/licenses/agpl-3.0.html)
--- gulpfile.js
+++ gulpfile.js
@@ -7,40 +7,33 @@ const argv = require('yargs').argv;
 const runSeq = require('run-sequence');
 const clean = require('gulp-clean');
 const eslint = require('gulp-eslint');
+const conventionalChangelog = require('gulp-conventional-changelog');
+const bump = require('gulp-bump');
+const gutil = require('gulp-util');
+const git = require('gulp-git');
+const fs = require('fs');
 
-gulp.task('default', ['lint', 'build']);
+gulp.task('default', ['lint', 'dist']);
 
 gulp.task('dev', () => {
-  return runSeq('build', 'server', 'watch');
+  return runSeq('dist', 'server', 'watch');
 });
 
-gulp.task('build', (callback) => {
+gulp.task('dist', (callback) => {
   return runSeq('clean', 'js', callback);
 });
 
 gulp.task('clean', () => {
-  return gulp.src('build/*', { read: false })
+  return gulp.src('dist/*', { read: false })
     .pipe(clean());
 });
 
 gulp.task('js', () => {
-  let stream = browserify({
-      entries: './src/index.js',
-      debug: true,
-      standalone: 'james'
-    })
-    .transform('babelify', { presets: ['es2015'] })
-    .bundle()
-    .pipe(source('james-admin-client.js'));
-
-  if (argv.production) {
-    stream = stream
-      .pipe(buffer())
-      .pipe(uglify());
-  }
-
-  return stream.pipe(gulp.dest('./build'));
+  return buildJs(argv.production);
 });
+
+gulp.task('js_dev', () => buildJs());
+gulp.task('js_pro', () => buildJs(true));
 
 gulp.task('lint', () => {
   return gulp.src(['gulpfile.js', 'src/**/*.js', 'examples/**/*.js'])
@@ -48,3 +41,114 @@ gulp.task('lint', () => {
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
 });
+
+gulp.task('changelog', () => {
+  return gulp.src('CHANGELOG.md', {
+    buffer: false
+  })
+    .pipe(conventionalChangelog({
+      preset: 'angular' // Or to any other commit message convention you use.
+    }))
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('bump-version', () => {
+  let type = 'patch';
+
+  if (argv.major) {
+    type = 'major';
+  } else if (argv.minor) {
+    type = 'minor';
+  }
+
+  return gulp.src(['./bower.json', './package.json'])
+    .pipe(bump({ type })
+    .on('error', gutil.log))
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('commit-changes', () => {
+  const version = getPackageJsonVersion();
+
+  return gulp.src('.')
+    .pipe(git.commit(`Bump version number to ${version}`, { args: '-a' }));
+});
+
+gulp.task('push-changes', (cb) => {
+  git.push('origin', 'master', cb);
+});
+
+gulp.task('create-release-tag', (cb) => {
+  const version = getPackageJsonVersion();
+
+  git.tag(`v${version}`, `Release version ${version}`, cb);
+});
+
+gulp.task('push-release-tag', (callback) => {
+  const version = getPackageJsonVersion();
+
+  git.push('origin', `refs/tags/v${version}`, callback);
+});
+
+gulp.task('commit-release', () => {
+  const version = getPackageJsonVersion();
+
+  return gulp.src('./dist')
+    .pipe(git.add({ args: '-f' }))
+    .pipe(git.commit(`release ${version}`));
+});
+
+gulp.task('create-release-branch', (callback) => {
+  const version = getPackageJsonVersion();
+
+  git.checkout(`release-${version}`, { args: '-b' }, callback);
+});
+
+gulp.task('release', (callback) => {
+  runSeq(
+    'bump-version',
+    'changelog',
+    'commit-changes',
+    'push-changes',
+    'create-release-branch',
+    'clean',
+    'js_dev',
+    'js_pro',
+    'commit-release',
+    'create-release-tag',
+    'push-release-tag',
+    (error) => {
+      if (error) {
+        gutil.log(error.message);
+      } else {
+        gutil.log('RELEASE FINISHED SUCCESSFULLY');
+      }
+      callback(error);
+    });
+});
+
+function getPackageJsonVersion() {
+  // We parse the json file instead of using require because require caches
+  // multiple calls so the version number won't be updated
+  return JSON.parse(fs.readFileSync('./package.json', 'utf8')).version;
+}
+
+function buildJs(production) {
+  const fileName = production ? 'james-admin-client.min.js' : 'james-admin-client.js';
+  let stream = browserify({
+      entries: './src/index.js',
+      debug: true,
+      standalone: 'james'
+    })
+    .transform('babelify', { presets: ['es2015'] })
+    .bundle()
+    .pipe(source(fileName));
+
+  if (production) {
+    stream = stream
+      .pipe(buffer())
+      .pipe(uglify());
+  }
+
+  return stream.pipe(gulp.dest('./dist'));
+}
