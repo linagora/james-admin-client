f325fb275d23acb92af35a4c38dc306035a7a59b
Tuan Tuan LE <tlcong@linagora.com>
2019-05-20 17:03:45 +0700

    linagora/lgs/openpaas/linagora.esn.james#12 remove wrong implementation for domain alias

--- src/api/domain_mappings.js
+++ src/api/domain_mappings.js
@@ -4,22 +4,6 @@ function mixin(client) {
   client.listDomainMappings = () => client.api({
     url: BASE_PATH
   });
-
-  client.listDomainAliases = sourceDomain => client.api({
-    url: `${BASE_PATH}/${sourceDomain}`
-  });
-
-  client.addDomainAlias = (sourceDomain, alias) => client.api({
-    url: `${BASE_PATH}/${sourceDomain}`,
-    method: 'PUT',
-    data: alias
-  });
-
-  client.removeDomainAlias = (sourceDomain, alias) => client.api({
-    url: `${BASE_PATH}/${sourceDomain}`,
-    method: 'DELETE',
-    data: alias
-  });
 }
 
 module.exports = mixin;
